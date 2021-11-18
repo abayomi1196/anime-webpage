8d6b7283268b5823d56339a620e352d86a1c1d29
Abayomi Ayodele <samabayomi301@gmail.com>
2021-11-18 19:05:06 +0100

    continue card styles & content

--- components/card/Card.js
+++ components/card/Card.js
@@ -5,20 +5,32 @@ import styles from "./Card.module.css";
 function Card({ anime }) {
   return (
     <a className={styles.card}>
-      <Image
-        src={anime.coverImage.extraLarge}
-        alt={anime.title.english}
-        width={"300px"}
-        height={"250px"}
-        objectFit={"cover"}
-      />
+      <div className={styles.cardImage}>
+        <Image
+          src={anime.coverImage.extraLarge}
+          alt={anime.title.english}
+          width={"450px"}
+          height={"350px"}
+          objectFit={"cover"}
+        />
 
-      {/* truncate the name, if its too long */}
-      {anime.title.userPreferred.length >= 30 ? (
-        <h4>{anime.title.userPreferred.slice(0, 30)}...</h4>
-      ) : (
-        <h4>{anime.title.userPreferred}</h4>
-      )}
+        <div className={styles.cardImage__text}>
+          <h4>{anime.title.userPreferred}</h4>
+          <p>{anime.genres.slice(0, 2).join(", ")}</p>
+        </div>
+      </div>
+
+      <div className={styles.cardText}>
+        <div className={styles.cardText__duration}>
+          <p>
+            <span>
+              {anime.startDate.year} - {anime.endDate.year || "PRESENT"}
+            </span>
+            {anime.episodes && <span>{anime.episodes} episodes</span>}
+          </p>
+          <p>{anime.duration} mins.</p>
+        </div>
+      </div>
     </a>
   );
 }
--- graphql/queries.js
+++ graphql/queries.js
@@ -5,12 +5,33 @@ export const GET_ANIMES = gql`
     Page(page: 1, perPage: 10) {
       media(type: $type, sort: $sort) {
         id
-        meanScore
+        status
+        genres
+        description
+        episodes
+        duration
+
         coverImage {
           extraLarge
         }
+
         title {
           userPreferred
+          english
+        }
+
+        studios {
+          nodes {
+            name
+          }
+        }
+
+        startDate {
+          year
+        }
+
+        endDate {
+          year
         }
       }
     }
